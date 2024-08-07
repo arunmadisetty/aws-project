03eae7c72a1012aea60e5897235ae39fd32973ea
arunmadisetty <arun826@live.com>
2024-08-06 19:24:19 -0500

    updated output.tf

--- output.tf
+++ output.tf
@@ -2,10 +2,10 @@ output "public_subnet_id" {
   value = aws_subnet.terraform.id
 }
 
-output "public_subnet_id" {
+output "public_subnet1_id" {
   value = aws_subnet.terraform1.id
 }
 
-output "public_subnet_id" {
+output "public_subnet2_id" {
   value = aws_subnet.terraform2.id
 }
